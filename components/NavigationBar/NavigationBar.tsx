9c37a1f7f04c83c78b67a0eedd077fc56f53e412
Franco Waisfeld <francowaisfeld@gmail.com>
2024-09-16 11:15:51 -0700

    dynamic styling for NavigationBar component Fixed bug where body and main HTML elements were not filling viewport width

--- components/NavigationBar/NavigationBar.tsx
+++ components/NavigationBar/NavigationBar.tsx
@@ -1,15 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import Styles from "./NavigationBar.module.css";
+import { FiAlignJustify } from "react-icons/fi";
+import { useState } from "react";
 
 const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <header className={Styles.header}>
+    <header className={Styles.header} onMouseLeave={handleMouseLeave}>
       <Link href="/">
         <div className={Styles.name}>Franco Waisfeld</div>
       </Link>
 
+      <div className={Styles.hamburger} onClick={toggleMenu}>
+        <FiAlignJustify />
+      </div>
+
       <nav>
-        <ul className={Styles.navigation}>
+        <ul className={`${Styles.navigation} ${isOpen ? Styles.active : ""}`}>
           <li className={Styles["navigation-links"]}>
             <Link href="/">About</Link>
           </li>
