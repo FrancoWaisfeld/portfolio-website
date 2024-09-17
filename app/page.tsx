b104920fb97c8d2849a7eecba2c00ca7e66472ee
Franco Waisfeld <francowaisfeld@gmail.com>
2024-09-17 11:31:58 -0700

    moved padding on main HTML tag to globals.css file

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@ import HeroSection from "@/components/HeroSection/HeroSection";
 import FullWidthLine from "@/components/FullWidthLine/FullWidthLine";
 import AboutSection from "@/components/AboutSection/AboutSection";
 import ContactSection from "@/components/ContactSection/ContactSection";
-import Styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={Styles.main}>
+    <main>
       <HeroSection />
       <FullWidthLine />
       <AboutSection />
